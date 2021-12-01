b310121a9074a94cbadf31bd138fb4558c0cfbdb
Yaroslav Fur <tapocki20@gmail.com>
2021-12-02 01:09:55 +0200

    GitIgnore Logger

---
+++ src/main/java/com/example/lab_08/classes/suppliers/CarConstructor.java
@@ -0,0 +1,47 @@
+package com.example.lab_08.classes.suppliers;
+
+import com.example.lab_08.classes.carParts.Accessory;
+import com.example.lab_08.classes.carParts.Body;
+import com.example.lab_08.classes.carParts.Car;
+import com.example.lab_08.classes.carParts.Engine;
+import com.example.lab_08.classes.warehouses.WarehouseController;
+import com.example.lab_08.interfaces.ICarBuilder;
+
+public class CarConstructor implements ICarBuilder {
+    private WarehouseController engineWarehouseController;
+    private WarehouseController bodyWarehouseController;
+    private WarehouseController accessoryWarehouseController;
+
+    public void reset() {
+        // to be upd
+    }
+
+    public CarConstructor(WarehouseController engineWC, WarehouseController bodyWC, WarehouseController accessoryWC) {
+        this.engineWarehouseController = engineWC;
+        this.bodyWarehouseController = bodyWC;
+        this.accessoryWarehouseController = accessoryWC;
+    }
+
+    @Override
+    public Engine getEngine() { return (Engine) engineWarehouseController.popItem(); }
+
+    @Override
+    public Body getBody() {
+        return (Body) bodyWarehouseController.popItem();
+    }
+
+    @Override
+    public Accessory getAccessory() { return (Accessory) accessoryWarehouseController.popItem(); }
+
+    @Override
+    public Car construct() {
+        Engine engine = getEngine();
+        Body body = getBody();
+        Accessory accessory = getAccessory();
+
+        if (engine == null || body == null || accessory == null)
+            return null;
+        else
+            return new Car(body, engine, accessory);
+    }
+}
